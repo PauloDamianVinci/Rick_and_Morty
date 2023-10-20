79475738dd0480dce93e9119b9660c1fc80e358e
PauloDamianVinci <paulodamianvinci@gmail.com>
2023-10-20 15:18:15 -0300

    version back

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -6,6 +6,8 @@ const postUser = require("../controllers/postUser");
 const login = require("../controllers/login");
 const postFav = require("../controllers/postFav");
 const deleteFav = require("../controllers/deleteFav");
+const getVersionBack = require("../controllers/getVersionBack");
+
 // rutas:
 router.get("/", getMain);
 router.get("/character/:id", getCharById);
@@ -13,5 +15,6 @@ router.get("/login", login);
 router.post("/login", postUser);
 router.post("/fav", postFav);
 router.put("/fav/:id", deleteFav);
+router.get("/versionback", getVersionBack);
 
 module.exports = router;
